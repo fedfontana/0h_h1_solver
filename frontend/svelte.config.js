e6c13f1e35fbb931ed4e5d39a86b37d9ca84430f
fedfontana <federico.fontana2000@gmail.com>
2022-04-19 13:45:59 +0200

    added ts stuff, will now add proper ts features

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,9 +1,15 @@
-import preprocess from 'svelte-preprocess';
 import adapter from '@sveltejs/adapter-auto';
+import preprocess from 'svelte-preprocess';
 import path from 'path';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+    preprocess: [
+        preprocess({
+            postcss: true
+        })
+    ],
+
     kit: {
         adapter: adapter(),
         vite: {
@@ -12,18 +18,11 @@ const config = {
                     $components: path.resolve('./src/components/'),
                     $src: path.resolve('./src/'),
                     $types: path.resolve('./src/types/'),
-                    $lib: path.resolve('./lib')
                 }
             }
 
         }
-    },
-
-    preprocess: [
-        preprocess({
-            postcss: true
-        })
-    ]
+    }
 };
 
 export default config;
