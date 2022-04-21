24e1039e32608c232d41bbddc09b3305f25c4098
fedfontana <federico.fontana2000@gmail.com>
2022-04-21 21:34:04 +0200

    removed old useless stuff

--- src/stores.ts
+++ src/stores.ts
@@ -4,10 +4,3 @@ export const error_message: Writable<string|null> = writable<string|null>(null);
 export const info_message: Writable<string|null> = writable<string|null>(null);
 export const success_message: Writable<string|null> = writable<string|null>(null);
 export const board_is_solution: Writable<boolean|null> = writable<boolean|null>(null);
-
-export function clearStores(): void {
-    error_message.set(null);
-    info_message.set(null);
-    success_message.set(null);
-    board_is_solution.set(null);
-}
